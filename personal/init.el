f32159281d7703985be4bad16dbf1a5402ffa8d5
Adrian Martin <adrian@pointslope.com>
2014-03-05 10:47:45 -0500

    Adding my init.el file to the personal dir

---
+++ personal/init.el
@@ -0,0 +1,15 @@
+;; Require these packages at startup
+(prelude-require-packages
+  '(
+   solarized-theme
+   git-gutter-fringe
+   smart-mode-line
+   dired+
+   ctags
+   ctags-update
+))
+
+
+;; Personal Deets
+(setq user-email-address "amartin@pointslope.com")
+(setq user-full-name "Adrian Martin")
